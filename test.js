72dd35f6cd75ae4a76f3ca75b0df463d19bd43c7
Nicolas McCurdy <thenickperson@gmail.com>
2013-05-21 17:18:25 -0400

    Turn test.js into a QUnit test suite and add a new test.html runner

--- test.js
+++ test.js
@@ -2,44 +2,32 @@
 /*jslint plusplus: true */
 /*jslint browser: true */
 
-/*global $, Gaddag, Scorer */
+/*global $, test, ok, _, Gaddag, Scorer */
 
 "use strict";
 
-(function () {
-  var gaddag = new Gaddag(),
-    scorer = new Scorer(),
-    wordArray = ["A", "AT", "CAR", "CAT", "CARE", "CARREL", "DATE", "PRECEDE", "PRESTO", "RADIUS"];
+var gaddag = new Gaddag(),
+  scorer = new Scorer(),
+  wordArray = ["A", "AT", "CAR", "CAT", "CARE", "CARREL", "DATE", "PRECEDE", "PRESTO", "RADIUS"];
 
-  function init() {
-    $("body").append('<h1>Tests</h1>');
-    $("body").append('<ul id="test-log"></ul>');
-  }
+test("Gaddag", function() {
+  ok(gaddag.addAll(wordArray), "should add words to its inner trie structure");
 
-  function log(message) {
-    $("#test-log").append("<li>" + message + "</li>");
-  }
+  ok(gaddag.getJson() === '{"A":{">":{"$":0,"T":0},"C":{">":{"R":{"$":0,"E":0,"R":{"E":{"L":0}}},"T":0}},"D":{">":{"T":{"E":0}}},"R":{">":{"D":{"I":{"U":{"S":0}}}}}},"T":{"A":{">":0,"C":{">":0},"D":{">":{"E":0}}},"S":{"E":{"R":{"P":{">":{"O":0}}}}}},"C":{">":{"A":{"R":{"$":0,"E":0,"R":{"E":{"L":0}}},"T":0}},"E":{"R":{"P":{">":{"E":{"D":{"E":0}}}}}}},"R":{"A":{"C":{">":{"$":0,"E":0,"R":{"E":{"L":0}}}}},"R":{"A":{"C":{">":{"E":{"L":0}}}}},"P":{">":{"E":{"C":{"E":{"D":{"E":0}}},"S":{"T":{"O":0}}}}},">":{"A":{"D":{"I":{"U":{"S":0}}}}}},"E":{"R":{"A":{"C":{">":0}},"R":{"A":{"C":{">":{"L":0}}}},"P":{">":{"C":{"E":{"D":{"E":0}}},"S":{"T":{"O":0}}}}},"T":{"A":{"D":{">":0}}},"C":{"E":{"R":{"P":{">":{"D":{"E":0}}}}}},"D":{"E":{"C":{"E":{"R":{"P":{">":0}}}}}}},"L":{"E":{"R":{"R":{"A":{"C":{">":0}}}}}},"D":{">":{"A":{"T":{"E":0}}},"E":{"C":{"E":{"R":{"P":{">":{"E":0}}}}}},"A":{"R":{">":{"I":{"U":{"S":0}}}}}},"P":{">":{"R":{"E":{"C":{"E":{"D":{"E":0}}},"S":{"T":{"O":0}}}}}},"S":{"E":{"R":{"P":{">":{"T":{"O":0}}}}},"U":{"I":{"D":{"A":{"R":{">":0}}}}}},"O":{"T":{"S":{"E":{"R":{"P":{">":0}}}}}},"I":{"D":{"A":{"R":{">":{"U":{"S":0}}}}}},"U":{"I":{"D":{"A":{"R":{">":{"S":0}}}}}}}', "should get its JSON representation");
 
-  function test() {
-    init();
+  ok(_.isEqual(gaddag.findWordsWithHook("U"), ["RADIUS"]), "should find all words with U");
+  ok(_.isEqual(gaddag.findWordsWithHook("E"), ["CARE", "CARREL", "PRECEDE", "PRESTO", "DATE", "PRECEDE", "PRECEDE"]), "should find all words with E");
+  ok(_.isEqual(gaddag.findWordsWithHook("D"), ["DATE", "PRECEDE", "RADIUS"]), "should find all words with D");
 
-    log("Adding words: " + wordArray.join(', '));
-    gaddag.addAll(wordArray);
+  ok(_.isEqual(gaddag.findWordsWithRackAndHook(["S", "T"], "U"), []), "should find all words with U that can be formed using S and T");
+  ok(_.isEqual(gaddag.findWordsWithRackAndHook(["S", "D", "T", "I", "A", "R"], "U"), ["RADIUS"]), "should find all words with U that can be formed using S, D, T, I, A and R");
+  ok(_.isEqual(gaddag.findWordsWithRackAndHook(["D", "P", "A", "T", "R", "O", "S"], "E"), ["PRESTO", "DATE"]), "should find all words with E that can be formed using D, P, A, T, R, O and S");
 
-    log("JSON string: " + gaddag.getJson());
-    log("All words with U: " + gaddag.findWordsWithHook('U').join(', '));
-    log("All words with E: " + gaddag.findWordsWithHook('E').join(', '));
-    log("All words with D: " + gaddag.findWordsWithHook('D').join(', '));
-    log("All words with U that can be formed using S and T: " + gaddag.findWordsWithRackAndHook(['S', 'T'], 'U').join(', '));
-    log("All words with U that can be formed using S, D, T, I, A and R: " + gaddag.findWordsWithRackAndHook(['S', 'D', 'T', 'I', 'A', 'R'], 'U').join(', '));
-    log("All words with E that can be formed using D, P, A, T, R, O and S: " + gaddag.findWordsWithRackAndHook(['D', 'P', 'A', 'T', 'R', 'O', 'S'], 'E').join(', '));
+  ok(_.isEqual(gaddag.findWordsWithRackAndHook(["D", "P", "A", "T", "R", "C", "E"], ""), ["A", "AT", "CAR", "CARE", "CAT", "DATE", "PRECEDE"]), 'should find all words that can be formed using ["D", "P", "A", "T", "R", "C", "E"] at start of game (no hook)');
+});
 
-    log("All words that can be formed using ['D', 'P', 'A', 'T', 'R', 'C', 'E'] at start of game (no hook): " + gaddag.findWordsWithRackAndHook(['D', 'P', 'A', 'T', 'R', 'C', 'E'], '').join(', '));
-
-    log("Score for RADIUS: " + scorer.score("RADIUS"));
-    log("Words sorted by score: " + scorer.sortByScore(wordArray).join(", "));
-    log("Best scoring word: " + scorer.findBestWord(wordArray));
-  }
-
-  test();
-}());
+test("Scorer", function() {
+  ok(scorer.score("RADIUS") === 7, "should find the score for RADIUS");
+  ok(_.isEqual(scorer.sortByScore(wordArray), ["PRECEDE", "PRESTO", "CARREL", "RADIUS", "CARE", "DATE", "CAT", "CAR", "AT", "A"]), "should sort the words by decreasing score");
+  ok(scorer.findBestWord(wordArray) === "PRECEDE", "should find the best scoring word");
+});
