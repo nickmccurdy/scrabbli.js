156ed05184487ac50990952900be67840d4a57b0
Nicolas McCurdy <thenickperson@gmail.com>
2013-05-21 02:04:13 -0400

    Modify test.js so it appends to a list in the DOM

--- gaddag/test.js
+++ gaddag/test.js
@@ -2,7 +2,7 @@
 /*jslint plusplus: true */
 /*jslint browser: true */
 
-/*global Gaddag */
+/*global $, Gaddag */
 
 "use strict";
 
@@ -10,16 +10,30 @@
   var gaddag = new Gaddag(),
     wordArray = ["A", "AT", "CAR", "CAT", "CARE", "CARREL", "DATE", "PRECEDE", "PRESTO", "RADIUS"];
 
-  console.log("Adding words: " + wordArray.join(', '));
-  gaddag.addAll(wordArray);
+  function init() {
+    $("body").append('<ul id="test-log"></ul>');
+  }
 
-  console.log("JSON string: " + gaddag.getJson());
-  console.log("All words with U: " + gaddag.findWordsWithHook('U').join(', '));
-  console.log("All words with E: " + gaddag.findWordsWithHook('E').join(', '));
-  console.log("All words with D: " + gaddag.findWordsWithHook('D').join(', '));
-  console.log("All words with U that can be formed using S and T: " + gaddag.findWordsWithRackAndHook(['S', 'T'], 'U').join(', '));
-  console.log("All words with U that can be formed using S, D, T, I, A and R: " + gaddag.findWordsWithRackAndHook(['S', 'D', 'T', 'I', 'A', 'R'], 'U').join(', '));
-  console.log("All words with E that can be formed using D, P, A, T, R, O and S: " + gaddag.findWordsWithRackAndHook(['D', 'P', 'A', 'T', 'R', 'O', 'S'], 'E').join(', '));
+  function log(message) {
+    $("#test-log").append("<li>" + message + "</li>");
+  }
 
-  console.log("All words that can be formed using ['D', 'P', 'A', 'T', 'R', 'C', 'E'] at start of game (no hook): " + gaddag.findWordsWithRackAndHook(['D', 'P', 'A', 'T', 'R', 'C', 'E'], '').join(', '));
+  function test() {
+    init();
+
+    log("Adding words: " + wordArray.join(', '));
+    gaddag.addAll(wordArray);
+
+    log("JSON string: " + gaddag.getJson());
+    log("All words with U: " + gaddag.findWordsWithHook('U').join(', '));
+    log("All words with E: " + gaddag.findWordsWithHook('E').join(', '));
+    log("All words with D: " + gaddag.findWordsWithHook('D').join(', '));
+    log("All words with U that can be formed using S and T: " + gaddag.findWordsWithRackAndHook(['S', 'T'], 'U').join(', '));
+    log("All words with U that can be formed using S, D, T, I, A and R: " + gaddag.findWordsWithRackAndHook(['S', 'D', 'T', 'I', 'A', 'R'], 'U').join(', '));
+    log("All words with E that can be formed using D, P, A, T, R, O and S: " + gaddag.findWordsWithRackAndHook(['D', 'P', 'A', 'T', 'R', 'O', 'S'], 'E').join(', '));
+
+    log("All words that can be formed using ['D', 'P', 'A', 'T', 'R', 'C', 'E'] at start of game (no hook): " + gaddag.findWordsWithRackAndHook(['D', 'P', 'A', 'T', 'R', 'C', 'E'], '').join(', '));
+  }
+
+  test();
 }());
