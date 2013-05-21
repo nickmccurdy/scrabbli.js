9960dd37076e7abbad028382ddafcde4c5e326ec
Nicolas McCurdy <thenickperson@gmail.com>
2013-05-21 18:34:01 -0400

    Add a button for building the GADDAG with the entire dictionary

---
+++ scrabbli.js
@@ -0,0 +1,13 @@
+/*jslint indent: 2 */
+/*jslint plusplus: true */
+/*jslint browser: true */
+
+/*global $, Gaddag, dictionary */
+
+"use strict";
+
+function init() {
+  var gaddag = new Gaddag();
+
+  gaddag.addAll(dictionary);
+}
