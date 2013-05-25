bdf611e910e2383a555343cd2cb47c4cb8c35912
Nicolas McCurdy <thenickperson@gmail.com>
2013-05-24 21:42:48 -0400

    Create a backbone model for a game of scrabbli

--- game.js
+++ game.js
@@ -1,9 +1,33 @@
-/*global $, Gaddag, dictionary */
+/*global $, _, Backbone, Gaddag, dictionary */
 
 "use strict";
 
-function init() {
-  var gaddag = new Gaddag();
+var Game = Backbone.Model.extend({
 
-  gaddag.add(dictionary);
+  defaults: {
+    board: [],
+    gaddag: new Gaddag()
+  },
+
+  initialize: function () {
+    this.create_board();
+    this.gaddag.add(dictionary());
+  },
+
+  create_board: function () {
+    var empty_row = [];
+
+    _.times(15, function () {
+      empty_row.append("");
+    });
+
+    _.times(15, function () {
+      this.board.append(empty_row);
+    });
+  }
+
+});
+
+function init() {
+  var game = new Game();
 }
