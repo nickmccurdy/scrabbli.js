66528ed8cc70564d5347c0f2d06ed41277d2b43f
Nicolas McCurdy <thenickperson@gmail.com>
2013-05-27 03:11:25 -0400

    Make Scorer a backbone model

--- src/scorer.js
+++ src/scorer.js
@@ -1,54 +1,57 @@
-/*global _ */
+/*global _, Backbone */
 
 "use strict";
 
-function Scorer() {
+var Scorer = Backbone.Model.extend({
 
-  var letterScores = {
-    " ": 0,
-    E: 1,
-    A: 1,
-    I: 1,
-    O: 1,
-    N: 1,
-    R: 1,
-    T: 1,
-    L: 1,
-    S: 1,
-    U: 1,
-    D: 2,
-    G: 2,
-    B: 3,
-    C: 3,
-    M: 3,
-    P: 3,
-    F: 4,
-    H: 4,
-    V: 4,
-    W: 4,
-    Y: 4,
-    K: 5,
-    J: 8,
-    X: 8,
-    Q: 10,
-    Z: 10
-  };
+  defaults: {
+    letterScores: {
+      " ": 0,
+      E: 1,
+      A: 1,
+      I: 1,
+      O: 1,
+      N: 1,
+      R: 1,
+      T: 1,
+      L: 1,
+      S: 1,
+      U: 1,
+      D: 2,
+      G: 2,
+      B: 3,
+      C: 3,
+      M: 3,
+      P: 3,
+      F: 4,
+      H: 4,
+      V: 4,
+      W: 4,
+      Y: 4,
+      K: 5,
+      J: 8,
+      X: 8,
+      Q: 10,
+      Z: 10
+    }
+  },
 
-  this.score = function (word) {
+  score: function (word) {
+    var that = this;
     return _.reduce(word, function (sum, letter) {
-      return sum + letterScores[letter];
+      return sum + that.get("letterScores")[letter];
     }, 0);
-  };
+  },
 
-  this.sortByScore = function (words) {
-    var scorer = new Scorer();
+  sortByScore: function (words) {
+    var that = this;
     return _.sortBy(words.sort().reverse(), function (word) {
-      return scorer.score(word);
+      return that.score(word);
     }).reverse();
-  };
+  },
 
-  this.findBestWord = function (words) {
+  findBestWord: function (words) {
     return this.sortByScore(words)[0];
-  };
+  }
 
-}
+});
